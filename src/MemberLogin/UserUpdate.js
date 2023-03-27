cddb145c347cccc73137a979b8cbb9952203ebf7
ankit jain <ankitj047@gmail.com>
2023-03-27 22:41:47 +0530

    userupdate state define

--- src/MemberLogin/UserUpdate.js
+++ src/MemberLogin/UserUpdate.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userSliceAction } from "../store/slices/userSlice";
 import { getregisterApiurl } from "../APICALL";
@@ -6,9 +6,20 @@ import Progressbar from "./Progressbar";
 import { useNavigate } from "react-router";
 import Commoncomponent from "./Commoncomponent";
 
+const initialstate = {
+  fname: "",
+  lname: "",
+  age: "",
+  email: "",
+  phone: "",
+  isaccept: false,
+  diseasedes: "",
+};
+
 export default function UserUpdate() {
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [state, setState] = useState(initialstate)
   const userdata = useSelector((state) => state.users.user);
 
   const getdata = async () => {
@@ -19,27 +30,64 @@ export default function UserUpdate() {
     dispatch(userSliceAction(data));
   };
 
-const handlsubmit =() => {
-  navigate('/familyupdate')
-} 
+  const handlsubmit = () => {
+    console.log("yes I am called");
+    navigate("/familyupdate");
+  };
 
   return (
     <>
-    <div><Commoncomponent/></div>
-    <div><Progressbar progress='0'/></div>
-      {/* <Progressbar bgcolor="red" progress='60'  height={30} />
-      <Progressbar bgcolor="#99ff66" progress='50'  height={30} />
-      <Progressbar bgcolor="#ff00ff" progress='85'  height={30} />
-      <Progressbar bgcolor="#99ccff" progress='95'  height={30} /> */}
-      <form onClick={()=> handlsubmit()}>
-        <div className="container">
+      <div>
+        <Commoncomponent />
+      </div>
+      <div>
+        <Progressbar progress="0" />
+      </div>
+      <form onClick={() => handlsubmit()}>
+        <div className="container userupdate-container">
           <div className="row">
             <div className="col-sm">
-              <input placeholder="Please enter First Name"/>
+              <input placeholder="Please enter First Name" />
             </div>
-            <div className="col-sm"><input  placeholder="Please enter Last Name"/></div>
+          </div>
+          <div className="row containerrow">
+            <div className="col-sm">
+              <input placeholder="Please enter Last Name" />
+            </div>
+          </div>
+          <div className="row containerrow">
+            <div className="col-sm">
+              <input placeholder="Please enter email" />
+            </div>
+          </div>
+          <div className="row containerrow">
+            <div className="col-sm">
+              <input placeholder="Please enter Phone number" />
+            </div>
+            <div className="col-sm">
+              <input placeholder="Please enter age" />
+            </div>
+          </div>
+          <div className="row containerrow">
+            <div className="col-sm">
+              <textarea
+                type="text"
+                rows="4"
+                cols="50"
+                placeholder="Please Describe Disease if Any"
+              />
+            </div>
+          </div>
+          <div className="row containerrow">
+            <div className="col-sm">
+              <input type="checkbox" placeholder="Please enter email" />
+            </div>
+            <div className="col-sm">
+              <label>I have read and Accept T&C</label>
+            </div>
           </div>
         </div>
+        <button>Submit</button>
       </form>
     </>
   );
